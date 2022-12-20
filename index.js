9e26af2f23226d84d0bbe008dc46a49e9e329bca
innoccent786 <57233663+innoccent786@users.noreply.github.com>
2022-12-20 22:20:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use((req,res,next)=>{
- res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
  next();
 })
 app.use(express.static('public'))
