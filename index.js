da1f96a6e8ff919a26de8fdc2f9ab51dedaa7106
innoccent786 <57233663+innoccent786@users.noreply.github.com>
2022-12-20 23:24:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,10 @@ app.use(cookieParser());
 //  next();
 // })
 app.use(function (req, res, next) {
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Origin", "*");
+// res.setHeader('Access-Control-Allow-Origin', '*');
+// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// res.setHeader('Access-Control-Allow-Credentials', true);
 next();
 });
 app.use(express.static('public'))
