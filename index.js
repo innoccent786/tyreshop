122e700703f9aa0fed7539a2c3561d0dca112120
innoccent786 <57233663+innoccent786@users.noreply.github.com>
2022-12-20 23:08:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ app.use(cookieParser());
 //  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 //  next();
 // })
-// app.use(function (req, res, next) {
-// res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-// res.setHeader('Access-Control-Allow-Credentials', true);
-// next();
-// });
+app.use(function (req, res, next) {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Credentials', true);
+next();
+});
 app.use(express.static('public'))
 app.use(Upload.single('file'));
 app.use(cors({
