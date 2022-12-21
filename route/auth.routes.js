5af36e47df02f84f6b0e8eaa24105c6949ccc505
innoccent786 <57233663+innoccent786@users.noreply.github.com>
2022-12-21 20:38:21 +0500

    Update auth.routes.js

--- route/auth.routes.js
+++ route/auth.routes.js
@@ -4,6 +4,6 @@ const authController=require('../controllers/auth.controller')
 const authMiddleware=require('../middlewares/auth')
 Router.post('/login',authController.login,authMiddleware.createJWT)
 Router.post('/logout',authMiddleware.verifyJWT,authController.logout)
-Router.get('/isLoggedIn',authMiddleware.verifyJWT,authController.isLoggedIn)
+Router.get('/isLoggedIn',authMiddleware.setHeadersCors,authMiddleware.verifyJWT,authController.isLoggedIn)
 
 module.exports=Router;
