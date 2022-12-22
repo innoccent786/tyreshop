8914cafe27cbe811f5e0a4726a0bf66ffe3dc114
innoccent786 <57233663+innoccent786@users.noreply.github.com>
2022-12-22 23:23:46 +0500

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,9 @@ const User=require('../models/user.model.js')
 const authMiddleware={ 
     setHeadersCors: async (req,res,next)=>{
         res.set('Access-Control-Allow-Origin', 'https://elevenstar.sofabespoke.co.uk')
+        res.set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,access_token, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+        res.set("Access-Control-Allow-Methods", "GET,DELETE,HEAD,OPTIONS,POST,PUT");
+        res.set('Access-Control-Allow-Credentials', 'true')
        next();
     },
     createJWT:async (req,res)=>{
